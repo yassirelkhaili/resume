633a19bca6ab7a41a402950886bbfc68decd9c48
yassirelkhaili <elkhailiyassir@gmail.com>
2023-12-17 20:37:42 +0100

    finished dark/light theme

---
+++ dist/sandbox.js
@@ -0,0 +1,71 @@
+"use strict";
+/**
+ * @author Yassir Elkhaili
+ * @license MIT
+*/
+let currenTheme = "";
+const handleInitialTheme = () => {
+    const rootClasses = ["transition", "duration-100"];
+    rootClasses.forEach((rootClass) => document.documentElement.classList.add(rootClass));
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+        currenTheme = "dark";
+    }
+    else if (!("color-theme" in localStorage)) {
+        localStorage.setItem("color-theme", "light");
+        currenTheme = "light";
+    }
+    else if (localStorage.getItem("color-theme") === "dark") {
+        document.documentElement.classList.add("dark");
+        currenTheme = "dark";
+    }
+};
+handleInitialTheme();
+document.addEventListener("DOMContentLoaded", () => {
+    const themeChanger = document.querySelector(".theme-changer");
+    const handleThemeChangerIcon = () => {
+        console.log("triggerded");
+        const oldthemeChangerSrc = themeChanger.getAttribute("src");
+        if (oldthemeChangerSrc) {
+            const lastSrcElement = oldthemeChangerSrc.split("/")[oldthemeChangerSrc.split("/").length - 1];
+            let newThemeChangerSrc = "";
+            lastSrcElement === "sun--v1.png" ? newThemeChangerSrc = oldthemeChangerSrc.replace(lastSrcElement, "bright-moon.png") : newThemeChangerSrc = oldthemeChangerSrc.replace(lastSrcElement, "sun--v1.png");
+            themeChanger.setAttribute("src", newThemeChangerSrc);
+        }
+    };
+    const changeImagesColor = () => {
+        console.log("triggered");
+        const images = document.querySelectorAll(".hobby-icon");
+        images.forEach((image) => {
+            const oldImageSrc = image.getAttribute("src");
+            if (oldImageSrc) {
+                let newImageSrc = "";
+                currenTheme === "dark" ? newImageSrc = oldImageSrc.replace(oldImageSrc.split("/")[5], "f8fafc") : newImageSrc = oldImageSrc.replace(oldImageSrc.split("/")[5], "707071");
+                image.setAttribute("src", newImageSrc);
+            }
+        });
+    };
+    changeImagesColor();
+    const themeChangeHandler = () => {
+        if (currenTheme === "light") {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("color-theme", "dark");
+            currenTheme = "dark";
+            changeImagesColor();
+            handleThemeChangerIcon();
+        }
+        else {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("color-theme", "light");
+            currenTheme = "light";
+            changeImagesColor();
+            handleThemeChangerIcon();
+        }
+    };
+    themeChanger && themeChanger.addEventListener("click", themeChangeHandler);
+    const initializeThemeChangerIcon = () => {
+        currenTheme === "light" ? themeChanger.setAttribute("src", "https://img.icons8.com/ios-filled/20/f8fafc/bright-moon.png") : themeChanger.setAttribute("src", "https://img.icons8.com/ios-filled/20/f8fafc/sun--v1.png");
+    };
+    initializeThemeChangerIcon();
+});
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./**/*.{html,js}",
+  "./dist/*.js"
+],
+darkMode: 'class',
+theme: {
+  fontFamily: {
+    'sourcesans': ['Source Sans Pro', 'sans-serif']
+  },
+  extend: {
+    colors: {
+      dark_background: "#111827",
+      dark_card_background: "#1F2937",
+      dark_color: "#6366F1",
+      main: '#323232',
+      sub: '#236FB2',
+    },
+  },
+},
+  plugins: [],
+}
+
